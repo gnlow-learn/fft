39e5c068ca67042a405535d50e05199b96cdb423
Gnlow <gnlow@protonmail.com>
2022-07-07 11:11:32 +0900

    benchmark

---
+++ test/polyMul.bench.ts
@@ -0,0 +1,35 @@
+import { assertEquals } from "https://deno.land/std/testing/asserts.ts"
+import {
+    Complex,
+    Vector,
+    fft,
+    ifft, 
+    slowFt,
+    iSlowFt
+} from "../mod.ts"
+
+Deno.bench("PolyMul: 4*4 - FFT", () => {
+    ifft(
+        fft([4,3,2,1,0,0,0,0])
+        .mul(fft([2,-1,3,-5,0,0,0,0]))
+    )+""
+})
+Deno.bench("PolyMul: 4*4 - SlowFT", () => {
+    iSlowFt(
+        slowFt([4,3,2,1,0,0,0,0])
+        .mul(slowFt([2,-1,3,-5,0,0,0,0]))
+    )+""
+})
+const [a, b] = Array(2).fill(undefined).map(() => Array(16).fill(undefined).map(() => Math.round(Math.random() * 20 - 10)))
+Deno.bench("PolyMul: 16*16 random - FFT", () => {
+    ifft(
+        fft(a)
+        .mul(fft(b))
+    )+""
+})
+Deno.bench("PolyMul: 16*16 random - SlowFT", () => {
+    iSlowFt(
+        slowFt(a)
+        .mul(slowFt(b))
+    )+""
+})
---
+++ test/polyMul.test.ts
@@ -0,0 +1,36 @@
+import { assertEquals } from "https://deno.land/std/testing/asserts.ts"
+import {
+    Complex,
+    Vector,
+    fft,
+    ifft, 
+    slowFt,
+    iSlowFt
+} from "../mod.ts"
+
+Deno.test("Polynomial Multiply - FFT", () => {
+    assertEquals(
+        fft([4,3,2,1].map(n => new Complex(n)))+"",
+        "10,2+2i,2,2-2i"
+    )
+    assertEquals(
+        ifft(
+            fft([4,3,2,1,0,0,0,0])          // x^3 + 2x^2 + 3x + 4
+            .mul(fft([2,-1,3,-5,0,0,0,0]))  // -5x^3 + 3x^2 - x + 2
+        )+"",
+        "8,2,13,-11,-10,-7,-5,0"
+    )
+})
+Deno.test("Polynomial Multiply - SlowFT", () => {
+    assertEquals(
+        slowFt([4,3,2,1].map(n => new Complex(n)))+"",
+        "10,2+2i,2,2-2i"
+    )
+    assertEquals(
+        iSlowFt(
+            slowFt([4,3,2,1,0,0,0,0])          // x^3 + 2x^2 + 3x + 4
+            .mul(slowFt([2,-1,3,-5,0,0,0,0]))  // -5x^3 + 3x^2 - x + 2
+        )+"",
+        "8,2,13,-11,-10,-7,-5,0"
+    )
+})
